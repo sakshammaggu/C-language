7fbf7e05d5dcf0c4a91f68757c88b5b752ea8713
Saksham Maggu <114247233+sakshammaggu@users.noreply.github.com>
2023-07-04 11:10:05 +0530

    Add files via upload

---
+++ program2.c
@@ -0,0 +1,12 @@
+//entering and adding two numbers
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    printf("enter no. a\n");
+    scanf("%d", &a);
+    printf("enter no. b\n");
+    scanf("%d", &b);
+    printf("result= %d\n", a+b);
+    return 0;
+}
