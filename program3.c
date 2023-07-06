152bc36df8f86f2bb4f8f1cff9b4cc85c5dabf19
Saksham Maggu <114247233+sakshammaggu@users.noreply.github.com>
2023-07-06 20:08:00 +0530

    Add files via upload

---
+++ program3.c
@@ -0,0 +1,12 @@
+//entering two nubers and subtracting it
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    printf("enter number a\n");
+    scanf("%d", &a);
+    printf("enter number b\n");
+    scanf("%d", &b);
+    printf("result= %d\n", a-b);
+    return 0;
+}
